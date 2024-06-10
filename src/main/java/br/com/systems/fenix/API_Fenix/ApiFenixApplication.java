2db5d93b0221c7f1e7918d49306a2a916336e4f2
Higor Miquilino <higor.miquilino@gmail.com>
2024-06-10 09:47:25 -0300

    First

---
+++ src/main/java/br/com/systems/fenix/API_Fenix/ApiFenixApplication.java
@@ -0,0 +1,13 @@
+package br.com.systems.fenix.API_Fenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFenixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/systems/fenix/API_Fenix/ApiFenixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systems.fenix.API_Fenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
